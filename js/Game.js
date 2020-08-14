1b310b20bc6f5cd1993f382f0d5a44b42de1ced2
trisha-n <trisha2006mummy@gmail.com>
2020-08-14 15:58:55 +0530

    added sprites

--- js/Game.js
+++ js/Game.js
@@ -15,13 +15,22 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
-      player = new Player();
-      player.getCount();
-      form = new Form()
+     player = new Player();
+     var playercountref = await database.ref('playerCount').once("value");
+     if(playercountref.exists()){
+       playerCount = playercountref.val();
+       player.getCount();     
+      } 
+      form = new Form();
       form.display();
     }
+    car1 = createSprite(100,200);
+    car2 = createSprite(300,200);
+    car3 = createSprite(500,200);
+    car4 = createSprite(700,200);
+    cars = [car1, car2, car3, car4];
   }
 
   play(){
@@ -30,21 +39,24 @@ class Game {
     text("GAME STARTS",120,100);
     Player.getAllPlayersInfo();
     if(allplayers !== undefined){
-      var displayPosition = 130;
+      var index = 0, x=0, y
       for(var plr in allplayers){
-        if(plr === "player" + player.index){
-          fill("red");
-        }else{
-          fill("black")
+        index = index + 1;
+        x = x+200;
+        y = displayHeight - allplayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+        if (index === player.index){
+          cars[index-1].shapeColor = "red";
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y;
         }
-        displayPosition = displayPosition + 20;
-        textSize(15);
-        text(allplayers[plr].name + " : " + allplayers[plr].distance, 120, displayPosition);
-      } 
-    }
+        }
+      }
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance += 50;
       player.update()
     }
+    drawSprites();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var canvas, backgroundImage;
 var allplayers;
-
+var car1, car2, car3 ,car4, cars;
 var gameState = 0;
 var playerCount;
 
@@ -10,7 +10,7 @@ var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth - 20 , displayHeight - 30);
   database = firebase.database();
   game = new Game();
   game.getState();
